205d32a3e811ea6a61fa22df93283da3ed7e14e3
sylym <qwejkl2222ccyy@126.com>
2022-07-05 12:54:49 +0800

    v1.2

--- simulation.py
+++ simulation.py
@@ -1,30 +1,33 @@
 """
-额外补充多穿补货规则：
-1.整版补货优先补充立库散拣剩余箱(剩余箱整版)
-2.优先填充该SKU数量最多的料箱
+额外补充多穿补货规则:
+1.整版补货优先补充立库散拣剩余箱(整版剩余箱)
+2.补货只能补到空料箱中
 3.触发补货时一次性补充足够的货
-额外补充立库散拣出库规则：
-1.立库散拣优先利用散拣剩余箱
-额外补充多穿料箱出库规则：
+额外补充立库散拣出库规则:
+1.立库散拣优先利用之前散拣剩余箱
+额外补充多穿料箱出库规则:
 1.料箱出库优先利用该SKU数量最少的料箱
 """
 import calendar
 import math
 import csv
+import copy
 
 NEED_MONTH = 6  # 订单月份
 
 
 SKU_DIC_TEMP = {}  # 减少使用次数 SKU编号(int)：[一箱支数(int)，一板箱数(int)，多穿料箱可放箱数(int)]
-PICKING_LIST = [[] for _ in range(calendar.monthrange(2021, NEED_MONTH)[1])]  # [[[SKU编号(int), 订单箱数(float), [一箱支数(int)，一板箱数(int)，多穿料箱可放箱数(int)]],..]，[第二天订单]]
 
+PICKING_LIST = [[] for _ in range(calendar.monthrange(2021, NEED_MONTH)[1])]
+# [[[SKU编号(int), 订单箱数(float), [一箱支数(int)，一板箱数(int)，多穿料箱可放箱数(int)]],..]，[第二天订单]]
 
-# PICKING_LIST的预处理
+# PICKING_LIST和SKU_DIC_TEMP的预处理
 SKU_INFO = csv.reader(open('sku_info_new.csv', 'r'))
 SKU_INFO = list(SKU_INFO)
 
 for PER_SKU in range(1, len(SKU_INFO)):
-    SKU_DIC_TEMP[int(SKU_INFO[PER_SKU][1])] = [int(SKU_INFO[PER_SKU][2]), int(SKU_INFO[PER_SKU][3]), int(SKU_INFO[PER_SKU][4])]
+    SKU_DIC_TEMP[int(SKU_INFO[PER_SKU][1])] = [int(SKU_INFO[PER_SKU][2]), int(SKU_INFO[PER_SKU][3]),
+                                               int(SKU_INFO[PER_SKU][4])]
 
 PICKING_HISTORY = csv.reader(open('picking_history.csv', 'r'))
 PICKING_HISTORY = list(PICKING_HISTORY)
@@ -54,6 +57,7 @@ for PER_PICKING in range(len(PICKING_HISTORY)-1, 0, -1):
 class Ms:
     def __init__(self, pr):
         self.sku_dic = {}  # SKU编号：多穿内箱数
+        self.sku_scattered_dic = {}  # SKU编号：[多穿货格零散箱数1, 多穿货格零散箱数2...]
         self.goods_cells_empty_num = 10320  # 多穿空货格数
         self.sell_supplement_num = 0  # 订单触发的补货数（累计）
         self.ms_sell_num = 0  # 多穿拣选的箱数（累计）
@@ -80,31 +84,19 @@ class Ms:
         if self.sku_dic[sku_id] >= sku_qty:
             self.sell_manage(sku_id, sku_qty, sku_info)
         else:
-            past_used_cells = math.ceil(self.sku_dic[sku_id] / sku_info[2])
-            self.goods_cells_empty_num += past_used_cells
+            self.sell_manage(sku_id, self.sku_dic[sku_id], sku_info)
             surplus_qty = sku_qty - self.sku_dic[sku_id]
-            self.sku_dic[sku_id] = 0
-            self.ms_sell_supplement(sku_id, surplus_qty, sku_info)
+            # 进行订单触发的补货
+            self.ms_supplement(sku_id, surplus_qty, sku_info, "sell")
             # 检测是否触发限制条件
             if self.goods_cells_empty_num < 0:
                 self.restriction = True
             self.sell_manage(sku_id, surplus_qty, sku_info)
 
-    # 订单触发的补货
-    def ms_sell_supplement(self, sku_id, supplement_num, sku_info):
-        self.sell_supplement_num += 1
-        if self.pr[sku_id] >= supplement_num:
-            self.supplement_manage(sku_id, self.pr[sku_id], sku_info)
-            self.pr[sku_id] = 0
-        else:
-            supplement_num -= self.pr[sku_id]
-            self.pr[sku_id] = 0
-            supplement_num = sku_info[1] * math.ceil(supplement_num / sku_info[1])
-            self.supplement_manage(sku_id, supplement_num, sku_info)
-
-    # min触发的补货
-    def ms_min_supplement(self, sku_id, ms_list, sku_info):
-        supplement_num = ms_list[sku_id][0] - self.sku_dic[sku_id]
+    # 订单和min触发的多穿补货
+    def ms_supplement(self, sku_id, supplement_num, sku_info, supplement_type):
+        if supplement_type == 'sell':
+            self.sell_supplement_num += 1
         if self.pr[sku_id] >= supplement_num:
             self.supplement_manage(sku_id, self.pr[sku_id], sku_info)
             self.pr[sku_id] = 0
@@ -123,48 +115,15 @@ class Mainwork:
         self.pr = {}  # SKU编号：立库散拣剩余箱
         self.ms = Ms(self.pr)
         self.ms.sku_dic = first_day_sku_dic.copy()  # 自定义初始多穿库存
-        # 计算初始多穿空货格数
+        # 计算初始多穿空货格数并填充sku_scattered_dic
         for sku_id in first_day_sku_dic:
             self.ms.goods_cells_empty_num -= math.ceil(self.ms.sku_dic[sku_id] / SKU_DIC_TEMP[sku_id][2])
-        # 前一天的数据
-        self.daytime_pre = 0
-        self.pr_pre = {}
-        self.sku_dic_pre = {}
-        self.goods_cells_empty_num_pre = 0
-        self.restriction_pre = False
-        self.total_sell_num_pre = 0
-        self.sell_supplement_num_pre = 0
-        self.ms_sell_num_pre = 0
+            self.ms.sku_scattered_dic[sku_id] = [self.ms.sku_dic[sku_id] % SKU_DIC_TEMP[sku_id][2]]
 
     # 添加每天的订单
     def add_day_picking(self):
         self.daytime += 1
         self.picking_list_day = PICKING_LIST[self.daytime - 1]
-
-    # 备份前一天的数据
-    def back_up(self):
-        self.daytime_pre = self.daytime
-        self.pr_pre = self.pr.copy()
-        self.sku_dic_pre = self.ms.sku_dic.copy()
-        self.goods_cells_empty_num_pre = self.ms.goods_cells_empty_num
-        self.restriction_pre = self.ms.restriction
-        self.total_sell_num_pre = self.total_sell_num
-        self.sell_supplement_num_pre = self.ms.sell_supplement_num
-        self.ms_sell_num_pre = self.ms.ms_sell_num
-        return self.daytime
-
-    # 恢复到前一天的数据
-    def restore(self):
-        self.daytime = self.daytime_pre
-        self.pr = self.pr_pre.copy()
-        self.ms.pr = self.pr
-        self.ms.sku_dic = self.sku_dic_pre.copy()
-        self.ms.goods_cells_empty_num = self.goods_cells_empty_num_pre
-        self.ms.restriction = self.restriction_pre
-        self.total_sell_num = self.total_sell_num_pre
-        self.ms.sell_supplement_num = self.sell_supplement_num_pre
-        self.ms.ms_sell_num = self.ms_sell_num_pre
-        return self.daytime
 
     # 立库出库
     def pr_sell(self, sku_id, sku_qty, ms_list, sku_info):
@@ -204,7 +163,7 @@ class Mainwork:
             # 订单出货
             if sku_id in ms_list:
                 if sku_qty >= 18:
-                    self.total_sell_num -= sku_qty
+                    self.total_sell_num -= sku_qty  # 计算总出货箱数不计算大于等于18箱的订单
                     self.pr_sell(sku_id, sku_qty, ms_list, sku_info)
                 else:
                     self.ms.ms_sell(sku_id, sku_qty, sku_info)
@@ -221,18 +180,21 @@ class Mainwork:
             # 进行min触发的补货
             if sku_id in ms_list:
                 if self.ms.sku_dic[sku_id] < ms_list[sku_id][0]:
-                    self.ms.ms_min_supplement(sku_id, ms_list, sku_info)
+                    self.ms.ms_supplement(sku_id, ms_list[sku_id][0] - self.ms.sku_dic[sku_id], sku_info, "min")
             # 检测是否触发限制条件
             if self.ms.goods_cells_empty_num < 0 or self.ms.restriction:
                 self.ms.restriction = True
                 break
         # 检测仿真是否结束
+        obs = [self.ms.goods_cells_empty_num, self.ms.sku_dic, self.pr, self.picking_list_day,
+               self.ms.sku_scattered_dic, self.daytime]
+        reward = [self.ms.ms_sell_num, self.ms.sell_supplement_num, self.total_sell_num]
         if self.ms.restriction:
-            return [self.ms.goods_cells_empty_num, self.ms.sku_dic, self.pr, self.picking_list_day], [self.ms.ms_sell_num, self.ms.sell_supplement_num, self.total_sell_num], True, "restriction!!!"
+            return obs, reward, True, "restriction!!!"
         elif self.daytime == calendar.monthrange(2021, NEED_MONTH)[1]:
-            return [self.ms.goods_cells_empty_num, self.ms.sku_dic, self.pr, self.picking_list_day], [self.ms.ms_sell_num, self.ms.sell_supplement_num, self.total_sell_num], True, "normal"
+            return obs, reward, True, "normal"
         else:
-            return [self.ms.goods_cells_empty_num, self.ms.sku_dic, self.pr, self.picking_list_day], [self.ms.ms_sell_num, self.ms.sell_supplement_num, self.total_sell_num], False, "normal"
+            return obs, reward, False, "normal"
 
 
 # 仿真接口
@@ -245,12 +207,5 @@ class SimulationEnv:
 
     def step(self, ms_list):
         obs, reward, done, info = self.work.start_work_step(ms_list)
-        return obs, reward, done, info
-
-    def restore(self):
-        daytime = self.work.restore()
-        return daytime
-
-    def backup(self):
-        daytime = self.work.back_up()
-        return daytime
+        # 返回obs的深拷贝会大幅度降低仿真性能, 如需要更快仿真速度, 可以改为直接返回obs, 但是不规范的使用可能obs可能导致出现仿真异常
+        return copy.deepcopy(obs), reward, done, info
